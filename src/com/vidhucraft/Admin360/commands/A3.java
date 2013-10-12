0663e7b7d3e9946bc7ddb09d4487953c47f2e214
Vidhu <xx.vidhuxx@gmail.com>
2013-10-12 15:55:39 -0400

    Added a3 command executer

---
+++ src/com/vidhucraft/Admin360/commands/A3.java
@@ -0,0 +1,21 @@
+package com.vidhucraft.Admin360.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/**
+ * This class is responsible for executing all commands
+ * that start with <pre>a3</pre> eg <pre>/a3 request</pre>
+ * @author Vidhu
+ *
+ */
+public class A3 implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		return false;
+	}
+
+}
