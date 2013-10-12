ad75515f603c3789d983704f25a245cd667a013a
Vidhu <xx.vidhuxx@gmail.com>
2013-10-12 02:23:47 -0400

    Added onEnable and onDisable Methods

--- src/com/vidhucraft/Admin360/Admin360.java
+++ src/com/vidhucraft/Admin360/Admin360.java
@@ -3,5 +3,14 @@ package com.vidhucraft.Admin360;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Admin360 extends JavaPlugin{
-
+	
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
 }
