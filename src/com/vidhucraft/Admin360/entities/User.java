acce74bd0168bc50293d55c107281bbc8e23f71f
Vidhu <xx.vidhuxx@gmail.com>
2013-10-13 03:04:09 -0400

    Added function to message player with preformated message

--- src/com/vidhucraft/Admin360/entities/User.java
+++ src/com/vidhucraft/Admin360/entities/User.java
@@ -1,5 +1,18 @@
 package com.vidhucraft.Admin360.entities;
 
-public class User {
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
+
+public class User {
+	
+	/**
+	 * Sends a message to all players in the server.
+	 * The message is preformated in this function so any message to
+	 * be sent can be done simply like this: <code>messagePlayers("hello world")</code>
+	 * @param msg <b>String</b> message to be sent
+	 */
+	public static void messagePlayers(String msg){
+		Bukkit.broadcastMessage(ChatColor.GREEN + msg);
+	}
 }
