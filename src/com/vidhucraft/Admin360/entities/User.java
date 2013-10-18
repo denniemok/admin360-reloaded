5615604c419be3a7fdf58547b1afe0be512163ab
Vidhu <xx.vidhuxx@gmail.com>
2013-10-18 01:28:51 -0400

    -Added method to allow sending messages to commandsender (enable to send messages to consoles)

--- src/com/vidhucraft/Admin360/entities/User.java
+++ src/com/vidhucraft/Admin360/entities/User.java
@@ -2,6 +2,8 @@ package com.vidhucraft.Admin360.entities;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 
 public class User {
@@ -13,6 +15,26 @@ public class User {
 	 * @param msg <b>String</b> message to be sent
 	 */
 	public static void messagePlayers(String msg){
-		Bukkit.broadcastMessage(ChatColor.GREEN + msg);
+		Bukkit.broadcastMessage(ChatColor.DARK_GREEN + "[Admin360] " + ChatColor.GREEN + msg);
+	}
+	
+	/**
+	 * Sends a message to the specified player with some preformating done
+	 * @param playerName
+	 * @param msg
+	 */
+	public static void messagePlayer(String playerName, String msg){
+		Player player = Bukkit.getPlayer(playerName);
+		player.sendMessage(ChatColor.GREEN + msg);
+	}
+	
+	/**
+	 * Sends a message to the specified Command sender (console/player) 
+	 * with some preformating done
+	 * @param commandsender
+	 * @param msg
+	 */
+	public static void messagePlayer(CommandSender commandsender, String msg){
+		commandsender.sendMessage(ChatColor.GREEN + msg);
 	}
 }
