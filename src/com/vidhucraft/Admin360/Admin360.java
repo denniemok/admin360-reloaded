b3cbe45a61d4df97bc754df424cc4dda0adb3cc5
Vidhu <xx.vidhuxx@gmail.com>
2013-10-18 01:29:12 -0400

    Fixed database setup method ordering

--- src/com/vidhucraft/Admin360/Admin360.java
+++ src/com/vidhucraft/Admin360/Admin360.java
@@ -3,9 +3,11 @@ package com.vidhucraft.Admin360;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.vidhucraft.Admin360.commands.A3;
+import com.vidhucraft.Admin360.datasource.DataSource;
+import com.vidhucraft.Admin360.datasource.SQLite_DataSource;
 
 public class Admin360 extends JavaPlugin{
-	
+	public static DataSource ds = new SQLite_DataSource();
 	
 	@Override
 	public void onEnable(){
@@ -14,6 +16,9 @@ public class Admin360 extends JavaPlugin{
 		getCommand("a3").setExecutor(a3);
 		getCommand("helpme").setExecutor(a3);
 		
+		//Connect to the database
+		ds.connect();
+		ds.setUp();
 	}
 	
 	@Override
