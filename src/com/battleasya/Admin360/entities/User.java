ba946cda272a25f769deb59cd41192a783156e96
Dennie Mok <mokdennie26@gmail.com>
2023-06-30 20:14:57 +1000

    Added the permission check function, cleaned up some comments

--- src/com/battleasya/Admin360/entities/User.java
+++ src/com/battleasya/Admin360/entities/User.java
@@ -1,7 +1,10 @@
 package com.battleasya.Admin360.entities;
 
+import com.battleasya.Admin360.util.Config;
+import com.battleasya.Admin360.util.Permission;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
@@ -11,22 +14,29 @@ public class User {
     public static HashMap<String, Long> coolDown = new HashMap<>();
 
     public static void messagePlayer(String playerName, String msg) {
-        //null pointer exception when the reminder is running
         try {
             Player player = Bukkit.getPlayer(playerName);
             player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
-        } catch (Exception e) {
+        } catch (Exception e) { // catch null pointer exception when the reminder is running
             Request.removePlayerRequest(playerName);
         }
+        
+    }
 
-        
-//        if (player == null) {
-//        	Request.removePlayerRequest(playerName);
-//        	return;
-//        }
-//
-//        player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
-        
+    // overloading method
+    public static void messagePlayer(CommandSender player, String msg) {
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+    }
+
+    public static boolean hasPermission(CommandSender sender, Permission permission, Boolean sendMsg){
+        if (!sender.hasPermission(permission.getString())) {
+            if (sendMsg) {
+                messagePlayer(sender, Config.noPermission);
+            }
+            return false;
+        } else {
+            return true;
+        }
     }
 
 }
