51bc8c174dc5345f4f8df94f9a14d59ddb0c0440
Dennie Mok <mokdennie26@gmail.com>
2023-06-30 20:16:00 +1000

    Modified to call datasource and handlers as an object from main class

--- src/com/battleasya/Admin360/commands/A3.java
+++ src/com/battleasya/Admin360/commands/A3.java
@@ -1,160 +1,171 @@
 package com.battleasya.Admin360.commands;
 
 import com.battleasya.Admin360.Admin360;
-import com.battleasya.Admin360.util.Permissions;
-import com.battleasya.Admin360.util.RequestHandler;
+import com.battleasya.Admin360.util.Config;
+import com.battleasya.Admin360.util.Permission;
 import com.battleasya.Admin360.entities.Request;
 import com.battleasya.Admin360.entities.User;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+/* This class handles everything about the /ticket command. */
+
 public class A3 implements CommandExecutor {
 
-    //public static Admin360 plugin = Admin360.getPlugin(Admin360.class);
     private final Admin360 plugin;
 
+    /* A3 Constructor */
     public A3 (Admin360 plugin) {
         this.plugin = plugin;
+    }
+
+    public void messageCommandList(CommandSender sender) {
+        for (String commands_player : Config.commandListPlayer) {
+            User.messagePlayer(sender, commands_player);
+        }
+        if (User.hasPermission(sender, Permission.RESPOND_TICKET, false)) {
+            for (String commands_staff : Config.commandListStaff) {
+                User.messagePlayer(sender, commands_staff);
+            }
+        }
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
+        // block command usage if the sender is not a player
         if(!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("is-player-check")));
+            User.messagePlayer(sender, Config.isPlayerCheck);
             return true;
         }
 
-        if (args.length == 0 || (args[0].equalsIgnoreCase("help") && args.length == 1)) {
-            for (String commands_player : plugin.getConfig().getStringList("command-list-player")) {
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', commands_player));
-            }
-            if (Permissions.hasPermission(sender, Permissions.RESPOND_TICKET, false)) {
-                for (String commands_staff : plugin.getConfig().getStringList("command-list-staff")) {
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', commands_staff));
-                }
-            }
+        if (args.length == 0) {
+            messageCommandList(sender);
             return true;
         }
 
         if (args.length == 1) {
 
+            if (args[0].equalsIgnoreCase("help")) {
+                messageCommandList(sender);
+                return true;
+            }
+
             if (args[0].equalsIgnoreCase("create")) {
-                if (Permissions.hasPermission(sender, Permissions.CREATE_TICKET, true)) {
-                    if (Permissions.hasPermission(sender, Permissions.RESPOND_TICKET, false)) {
-                        User.messagePlayer(sender.getName(), plugin.getConfig().getString("create-failed-anti-exploit"));
+                if (User.hasPermission(sender, Permission.CREATE_TICKET, true)) {
+                    if (User.hasPermission(sender, Permission.RESPOND_TICKET, false)) {
+                        User.messagePlayer(sender, Config.create_failed_anti_exploit);
                         return true;
                     }
-                    RequestHandler.createTicket(sender.getName(), "NULL");
+                    plugin.getRequestHandler().createTicket(sender.getName(), "NULL");
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("cancel")) {
-                if (Permissions.hasPermission(sender, Permissions.CREATE_TICKET, true)) {
-                    RequestHandler.cancelTicket(sender.getName());
+                if (User.hasPermission(sender, Permission.CREATE_TICKET, true)) {
+                    plugin.getRequestHandler().cancelTicket(sender.getName());
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("status")) {
-                if (Permissions.hasPermission(sender, Permissions.VIEW_STATUS, true)) {
-                    RequestHandler.printTicketStatus(sender.getName());
+                if (User.hasPermission(sender, Permission.VIEW_STATUS, true)) {
+                    plugin.getRequestHandler().printTicketStatus(sender.getName());
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("stats")) {
-                if (Permissions.hasPermission(sender, Permissions.VIEW_STATS, true)) {
-                    RequestHandler.printTicketStats(sender.getName());
+                if (User.hasPermission(sender, Permission.VIEW_STATS, true)) {
+                    plugin.getRequestHandler().printTicketStats(sender.getName());
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("list")) {
-                if (Permissions.hasPermission(sender, Permissions.RESPOND_TICKET, true)) {
-                    RequestHandler.printQueueList(sender.getName());
+                if (User.hasPermission(sender, Permission.RESPOND_TICKET, true)) {
+                    plugin.getRequestHandler().printQueueList(sender.getName());
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("next")) {
-                if (Permissions.hasPermission(sender, Permissions.RESPOND_TICKET, true)) {
-                    RequestHandler.nextTicket(sender.getName());
+                if (User.hasPermission(sender, Permission.RESPOND_TICKET, true)) {
+                    plugin.getRequestHandler().nextTicket(sender.getName());
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("tp")) {
-                if (Permissions.hasPermission(sender, Permissions.TELEPORT, true)) {
-                    RequestHandler.teleportPlayer(sender.getName());
+                if (User.hasPermission(sender, Permission.TELEPORT, true)) {
+                    plugin.getRequestHandler().teleportPlayer(sender.getName());
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("info")) {
-                if (Permissions.hasPermission(sender, Permissions.VIEW_INFO, true)) {
-                    RequestHandler.printTicketDetails(sender.getName());
+                if (User.hasPermission(sender, Permission.VIEW_INFO, true)) {
+                    plugin.getRequestHandler().printTicketDetails(sender.getName());
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("drop")) {
-                if (Permissions.hasPermission(sender, Permissions.DROP_TICKET, true)) {
-                    RequestHandler.dropTicket(sender.getName());
+                if (User.hasPermission(sender, Permission.DROP_TICKET, true)) {
+                    plugin.getRequestHandler().dropTicket(sender.getName());
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("close")) {
-                if (Permissions.hasPermission(sender, Permissions.RESPOND_TICKET, true)) {
-                    RequestHandler.closeTicket(sender.getName());
+                if (User.hasPermission(sender, Permission.RESPOND_TICKET, true)) {
+                    plugin.getRequestHandler().closeTicket(sender.getName());
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("yes")) {
-                if (Permissions.hasPermission(sender, Permissions.CREATE_TICKET, true)) {
-                    RequestHandler.requestFeedback(sender.getName(), true);
+                if (User.hasPermission(sender, Permission.CREATE_TICKET, true)) {
+                    plugin.getRequestHandler().requestFeedback(sender.getName(), true);
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("no")) {
-                if (Permissions.hasPermission(sender, Permissions.CREATE_TICKET, true)) {
-                    RequestHandler.requestFeedback(sender.getName(), false);
+                if (User.hasPermission(sender, Permission.CREATE_TICKET, true)) {
+                    plugin.getRequestHandler().requestFeedback(sender.getName(), false);
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("purge")) {
-                if (Permissions.hasPermission(sender, Permissions.PURGE_TICKET, true)) {
-                    RequestHandler.purgeTicket(sender.getName());
+                if (User.hasPermission(sender, Permission.PURGE_TICKET, true)) {
+                    plugin.getRequestHandler().purgeTicket(sender.getName());
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("hpstats")) {
-                if (Permissions.hasPermission(sender, Permissions.VIEW_HP_STATS, true)) {
+                if (User.hasPermission(sender, Permission.VIEW_HP_STATS, true)) {
                     String playerName = sender.getName();
-                    RequestHandler.printHonorStats(playerName, sender.getName());
+                    plugin.getRequestHandler().printHonorStats(playerName, sender.getName());
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("hptop")) {
-                if (Permissions.hasPermission(sender, Permissions.VIEW_HP_TOP, true)) {
-                    RequestHandler.printHonorTop(sender.getName(), plugin.getConfig().getInt("default-leaderboard-output"));
+                if (User.hasPermission(sender, Permission.VIEW_HP_TOP, true)) {
+                    plugin.getRequestHandler().printHonorTop(sender.getName(), Config.default_leaderboard_output);
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("history")) {
-                if (Permissions.hasPermission(sender, Permissions.VIEW_HISTORY, true)) {
-                    RequestHandler.printHonorHistory(sender.getName(), plugin.getConfig().getInt("default-history-output"));
+                if (User.hasPermission(sender, Permission.VIEW_HISTORY, true)) {
+                    plugin.getRequestHandler().printHonorHistory(sender.getName(), Config.default_history_output);
                 }
                 return true;
             }
@@ -165,65 +176,65 @@ public class A3 implements CommandExecutor {
         if (args.length == 2) {
 
             if (args[0].equalsIgnoreCase("pick")) {
-                if (Permissions.hasPermission(sender, Permissions.RESPOND_TICKET, true)) {
-                    RequestHandler.pickTicket(sender.getName(), args[1]);
+                if (User.hasPermission(sender, Permission.RESPOND_TICKET, true)) {
+                    plugin.getRequestHandler().pickTicket(sender.getName(), args[1]);
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("redirect")) {
-                if (Permissions.hasPermission(sender, Permissions.REDIRECT_TICKET, true)) {
-                    RequestHandler.redirectTicket(sender.getName(), args[1]);
+                if (User.hasPermission(sender, Permission.REDIRECT_TICKET, true)) {
+                    plugin.getRequestHandler().redirectTicket(sender.getName(), args[1]);
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("delete")) {
-                if (Permissions.hasPermission(sender, Permissions.DELETE_TICKET, true)) {
+                if (User.hasPermission(sender, Permission.DELETE_TICKET, true)) {
                     Request.removePlayerRequest(args[1]);
-                    User.messagePlayer(sender.getName(), plugin.getConfig().getString("delete-message")
+                    User.messagePlayer(sender, Config.delete_message
                             .replaceAll("<PLAYERNAME>", args[1]));
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("hpreset")) {
-                if (Permissions.hasPermission(sender, Permissions.RESET_HP_STATS, true)) {
-                    if (Admin360.ds.resetAdminsHonor(args[1])) {
-                        User.messagePlayer(sender.getName(), plugin.getConfig().getString("reset-hpstats-succeeded")
+                if (User.hasPermission(sender, Permission.RESET_HP_STATS, true)) {
+                    if (plugin.getDataSource().resetAdminsHonor(args[1])) {
+                        User.messagePlayer(sender, Config.reset_hpstats_succeeded
                                 .replaceAll("<ADMINNAME>", args[1]));
                     } else {
-                        User.messagePlayer(sender.getName(), plugin.getConfig().getString("reset-hpstats-failed"));
+                        User.messagePlayer(sender, Config.reset_hpstats_failed);
                     }
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("hpstats")) {
-                if (Permissions.hasPermission(sender, Permissions.VIEW_HP_STATS, true)) {
+                if (User.hasPermission(sender, Permission.VIEW_HP_STATS, true)) {
                     String playerName = args[1];
-                    RequestHandler.printHonorStats(playerName, sender.getName());
+                    plugin.getRequestHandler().printHonorStats(playerName, sender.getName());
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("hptop")) {
-                if (Permissions.hasPermission(sender, Permissions.VIEW_HP_TOP, true)) {
+                if (User.hasPermission(sender, Permission.VIEW_HP_TOP, true)) {
                     try {
-                        RequestHandler.printHonorTop(sender.getName(), Integer.parseInt(args[1]));
+                        plugin.getRequestHandler().printHonorTop(sender.getName(), Integer.parseInt(args[1]));
                     } catch (Exception e) {
-                        User.messagePlayer(sender.getName(), plugin.getConfig().getString("incorrect-syntax"));
+                        User.messagePlayer(sender, Config.incorrectSyntax);
                     }
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("history")) {
-                if (Permissions.hasPermission(sender, Permissions.VIEW_HISTORY, true)) {
+                if (User.hasPermission(sender, Permission.VIEW_HISTORY, true)) {
                     try {
-                        RequestHandler.printHonorHistory(sender.getName(), Integer.parseInt(args[1]));
+                        plugin.getRequestHandler().printHonorHistory(sender.getName(), Integer.parseInt(args[1]));
                     } catch (Exception e) {
-                        User.messagePlayer(sender.getName(), plugin.getConfig().getString("incorrect-syntax"));
+                        User.messagePlayer(sender, Config.incorrectSyntax);
                     }
                 }
                 return true;
@@ -232,21 +243,21 @@ public class A3 implements CommandExecutor {
         }
 
         if (args[0].equalsIgnoreCase("create")) {
-            if (Permissions.hasPermission(sender, Permissions.CREATE_TICKET, true)) {
-                if (Permissions.hasPermission(sender, Permissions.RESPOND_TICKET, false)) {
-                    User.messagePlayer(sender.getName(), plugin.getConfig().getString("create-failed-anti-exploit"));
+            if (User.hasPermission(sender, Permission.CREATE_TICKET, true)) {
+                if (User.hasPermission(sender, Permission.RESPOND_TICKET, false)) {
+                    User.messagePlayer(sender, Config.create_failed_anti_exploit);
                     return true;
                 }
                 StringBuilder reason = new StringBuilder();
                 for (int i = 1; i < args.length; i++) {
                     reason.append(" ").append(args[i]);
                 }
-                RequestHandler.createTicket(sender.getName(), reason.toString());
+                plugin.getRequestHandler().createTicket(sender.getName(), reason.toString());
             }
             return true;
         }
 
-        User.messagePlayer(sender.getName(), plugin.getConfig().getString("incorrect-syntax"));
+        User.messagePlayer(sender, Config.incorrectSyntax);
         return true;
 
     }
--- src/com/battleasya/Admin360/commands/B3.java
+++ src/com/battleasya/Admin360/commands/B3.java
@@ -1,16 +1,20 @@
 package com.battleasya.Admin360.commands;
 
 import com.battleasya.Admin360.Admin360;
-import com.battleasya.Admin360.util.Permissions;
-import org.bukkit.ChatColor;
+import com.battleasya.Admin360.entities.User;
+import com.battleasya.Admin360.util.Config;
+import com.battleasya.Admin360.util.Permission;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+
+/* This class handles everything about the /admin360 command. */
 
 public class B3 implements CommandExecutor {
 
     private final Admin360 plugin;
 
+    /* B3 Constructor */
     public B3(Admin360 plugin) {
         this.plugin = plugin;
     }
@@ -26,39 +30,39 @@ public class B3 implements CommandExecutor {
         if (args.length == 1) {
 
             if (args[0].equalsIgnoreCase("help")) {
-                for (String commands_player : plugin.getConfig().getStringList("command-list-player")) {
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', commands_player));
+                for (String commands_player : Config.commandListPlayer) {
+                    User.messagePlayer(sender, commands_player);
                 }
-                if (Permissions.hasPermission(sender, Permissions.RESPOND_TICKET, false)) {
-                    for (String commands_staff : plugin.getConfig().getStringList("command-list-staff")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', commands_staff));
+                if (User.hasPermission(sender, Permission.RESPOND_TICKET, false)) {
+                    for (String commands_staff : Config.commandListStaff) {
+                        User.messagePlayer(sender, commands_staff);
                     }
                 }
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("reload")) {
-                if (Permissions.hasPermission(sender, Permissions.RELOAD_CONFIG, true)) {
+                if (User.hasPermission(sender, Permission.RELOAD_CONFIG, true)) {
                     plugin.reloadConfig();
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                            plugin.getConfig().getString("reload-message")));
+                    plugin.getConfig2().fetchConfig();
+                    User.messagePlayer(sender, Config.reloadMessage);
                 }
                 return true;
             }
 
         }
 
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("incorrect-syntax")));
+        User.messagePlayer(sender, Config.incorrectSyntax);
         return true;
 
     }
 
-    public static void printInfo(CommandSender sender) {
-        sender.sendMessage("");
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&f[&6ADMIN360&f] Version: &78.1.2"));
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&f[&6ADMIN360&f] Authors: &7Vidhu, Dennie"));
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&f[&6ADMIN360&f] Project: &7https://cutt.ly/bjo1zpy"));
-        sender.sendMessage("");
+    public void printInfo(CommandSender sender) {
+        User.messagePlayer(sender, "");
+        User.messagePlayer(sender, "&f[&6ADMIN360&f] Version: &7" + plugin.getDescription().getVersion());
+        User.messagePlayer(sender, "&f[&6ADMIN360&f] Authors: &7Vidhu, Dennie");
+        User.messagePlayer(sender, "&f[&6ADMIN360&f] Project: &7https://cutt.ly/bjo1zpy");
+        User.messagePlayer(sender, "");
     }
 
 }
